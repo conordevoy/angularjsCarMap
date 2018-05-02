c72f50d532f3ee2474871e50ab00a7130ddbd836
Conor Devoy <conor.devoy@gmail.com>
2018-05-02 22:08:49 +0100

    move some code around

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular.module('app', ["ngRoute"]);
+var app = angular.module('app', ["ngRoute"]); // the app (1)
 
 app.config(function($routeProvider) {
 	$routeProvider
@@ -12,13 +12,13 @@ app.config(function($routeProvider) {
 	});
 });
 
-app.factory('DataService', function() {
+app.factory('DataService', function() { // the service (3)
 	
 	var dataService = {};
 
 	var data = [];
 
-	dataService.changeData = function(d) {
+	dataService.addData = function(d) {
 		data.push({reg: d.reg, lat: d.lat, lon:d.lon});
 		localStorage.setItem('data', JSON.stringify(data));
 	}
@@ -31,24 +31,28 @@ app.factory('DataService', function() {
 	return dataService;
 });
 
-app.controller('form-input', function($scope, DataService) {
+app.controller('form-input', function($scope, DataService) { // controller for page 1 (2)
 
-	$scope.submitForm = function() {
+	$scope.submitForm = function() { // gets the form input (4)
 		let data = {reg: $scope.reg, lat: $scope.lat, lon: $scope.lon};
 		$scope.updateData(data);
 	};
 
 	$scope.updateData = function(data) {
-		DataService.changeData(data)
+		DataService.addData(data) // sends form input to service (4)
 		$scope.data = DataService.getData();
 	};
 });
 
-app.controller('mapping', function($scope, DataService) {
+app.controller('mapping', function($scope, DataService) { // controller for page 2 (2)
 
-	$scope.data = DataService.getData();
+	var map = L.map('map').setView([53, -7.2], 6);
+	map.zoomControl.setPosition('topright');
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-	if ($scope.data) {
+	$scope.data = DataService.getData(); // getsdata from service
+
+	if ($scope.data) { // plots data on map (5)
 		for (var i = 0; i < $scope.data.length; i++) { 
 		    L.marker([parseFloat($scope.data[i].lat), parseFloat($scope.data[i].lon)]).addTo(map);
 		}
